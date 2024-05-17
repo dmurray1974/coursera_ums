d5e67886357836a65c2fe878de1f63f413852924
dmurray <mcs.ltd@gmail.com>
2024-05-17 08:43:37 -0400

    Adding temporary changes to Github

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://mcsltd:MTM3MjMtbWNzbHRk@127.0.0.1:27017');
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
